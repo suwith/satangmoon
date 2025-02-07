0c7ae5e5cdb4e01ab206d5cc75ac02dc0c131f79
suwith <suwith@kookmin.ac.kr>
2025-02-07 14:40:02 +0900

    tailwind init

---
+++ src/hooks/useChocolate.js
@@ -0,0 +1,3 @@
+const useChocolate = () => {
+return 0;
+};
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+      <div className="flex flex-col items-center text-center bg-[#fffad0] h-screen w-full">
+    <div className="text-xl ">홈 페이지</div>
+          {/* 컨텐츠 추가 */}
+      </div>
+    );
+};
+
+export default HomePage;
