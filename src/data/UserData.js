1a2b1b848d47e7d65c2555bdd923f1e5b4fd4750
suwith <suwith@kookmin.ac.kr>
2025-02-07 20:02:34 +0900

    Revert "Revert "[feat] 초콜릿 배치 및 초콜릿함 페이지네이션 완료 #2""
    This reverts commit 1331c1e69fb1e5258d9c214a4b7f3d3437553575.

---
+++ src/data/UserData.js
@@ -0,0 +1,17 @@
+export const users = [
+  {
+    id: "1",
+    kakao_id: "kakao_123",
+    real_name: "김철수"
+  },
+  {
+    id: "2",
+    kakao_id: "kakao_456",
+    real_name: "이영희"
+  },
+  {
+    id: "3",
+    kakao_id: "kakao_789",
+    real_name: "박민수"
+  }
+];
