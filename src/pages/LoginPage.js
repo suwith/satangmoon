c71946c922e9e4141c28a4c6edf38c8eefea1907
HYEJIN <imagine@kookmin.ac.kr>
2025-03-09 14:38:18 +0900

    [feat] 로그인 된 유저는 바로 `/home`으로 이동

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,15 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import candyLogo from "../assets/candy_logo.svg";
 import useLogin from "../hooks/useLogin";
+import { decodeUserInfo } from '../utils/UserUtils';
+import { useEffect } from "react";
 
 const LoginPage = () => {
   const navigate = useNavigate(); // useNavigate 훅 사용
   const { kakaoLogin } = useLogin();
 
+  useEffect(() => {
+    const decodedUser = decodeUserInfo();
+    if (decodedUser) {
+      navigate('/home');
+    }
+  }, [navigate]);
+
+
+  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
-
-
       {/* 하트 아이콘 */}
       <div className="relative w-5/6 flex justify-center mb-16">
         <img src={candyLogo} alt="candyLogo" className="w-full h-auto object-contain" />
