34fbf1e88924f7f2fb147d8b1ba5167997de9eab
suwith <suwith@kookmin.ac.kr>
2025-03-12 03:12:44 +0900

    로그인 테스트3 - 프론트 callback 라우팅 수정

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/home" element={<HomePage />} />
             <Route path="/:id" element={<HomePage />} />
-            <Route path="/oauth/callback" element={<KakaoCallback />} />
+            <Route path="/oauth/callback/kakao" element={<KakaoCallback />} />
           </Routes>
         </div>
       </Router>
